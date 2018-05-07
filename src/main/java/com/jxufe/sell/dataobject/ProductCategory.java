d853aaf347e2afcba9088cdf3aa0410f8e816c4a
Br1ckman <lizhengfanlt@163.com>
2018-05-07 21:35:41 +0800

    第四章（2）

---
+++ src/main/java/com/jxufe/sell/dataobject/ProductCategory.java
@@ -0,0 +1,76 @@
+package com.jxufe.sell.dataobject;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class ProductCategory {
+
+    //类目ID
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer categoryId;
+
+    //类目名字
+    private String categoryName;
+
+    //类目编号
+    private Integer categoryType;
+
+    private Date createTime;
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    private Date updateTime;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(Integer categoryType) {
+        this.categoryType = categoryType;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", categoryType='" + categoryType + '\'' +
+                '}';
+    }
+}
